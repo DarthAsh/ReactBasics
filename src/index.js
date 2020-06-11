503e5800981491c21ff84eeff3ddb406aae76899
DarthAsh <54763573+DarthAsh@users.noreply.github.com>
2020-06-11 11:17:49 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+import React from "react";
+import ReactDom from "react-dom";
+
+var d = new Date();
+const name = "Ashwin";
+ReactDom.render(
+  <div>
+    <p>Created by {name}</p>
+    <p>Copyright {d.getFullYear()}</p>
+  </div>,
+  document.getElementById("root")
+);
